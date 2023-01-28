aaa4b2dab019971cda45a33e87cf80e9100be235
yude <i@yude.jp>
2023-01-28 19:40:21 +0900

    Cleanup & @1.0.1

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 let url = new URL(window.location.href);
+
+let l = document.links;
+
+let video_title = document.title.replace(" | Iwara", "")
+let author_class = document.querySelector('.username')
+let author = author_class.innerHTML
+let file_name = (author + " - " + video_title + ".mp4").replaceAll("/", "／")
+
 let btn = document.createElement("button");
 btn.innerHTML = "元画質でダウンロード";
+let textbox = document.createElement("textarea")
+textbox.innerHTML = file_name
 
 btn.onclick = function () {
-    let l = document.links;
     for (let i = 0; i < l.length; i++) {
         if (l[i].href.indexOf("Source.mp4") > -1) {
             let target = l[i].href
-
-            let video_title = document.title.replace(" | Iwara", "")
-            let author_class = document.querySelector('.username')
-            let author = author_class.innerHTML
-            let file_name = (author + " - " + video_title + ".mp4").replaceAll("/", "／")
 
             browser.runtime.sendMessage({
                 "action": "download_file",
@@ -22,7 +26,9 @@ btn.onclick = function () {
             break;
         }
     }
+
 }
 
 let parent = document.querySelector('.node-buttons')
 parent.appendChild(btn);
+parent.appendChild(textbox)
